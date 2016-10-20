1b81f37347cc3846c4ac764e9e4c6767e61f59d8
Amy Buck <amy_buck@dell.com>
2016-10-20 07:57:15 -0700

    Update create-mac-table-entry.py

--- create-mac-table-entry.py
+++ create-mac-table-entry.py
@@ -1,4 +1,5 @@
-### Configure MAC address table entry
+# Python code block to create MAC address table entry
+
 import   cps_utils
 
 cps_utils.add_attr_type("base-mac/table/mac-address",   "mac")
@@ -7,8 +8,10 @@ d  =     {"mac-address":   "00:0a:0b:cc:0d:0e", "ifindex":   18, "vlan":   "100"
 obj   =  cps_utils.CPSObject('base-mac/table',data=   d)
 tr_obj   =  ('create',   obj.get())
 
+# Commit the transaction
 transaction   =  cps_utils.CPSTransaction([tr_obj])
 ret   =  transaction.commit()
 
+# Check for failure
 if not   ret:
     raise   RuntimeError   ("Error   creating   MAC   Table   Entry") 
