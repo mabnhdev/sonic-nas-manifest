13e416923ba46bd186b396d336efde0d1a2f4440
Amy Buck <amy_buck@dell.com>
2016-10-20 08:06:18 -0700

    Update publish-events.c

--- publish-events.c
+++ publish-events.c
@@ -21,11 +21,11 @@ bool   cps_pub_intf_event()   {
   }
 
 
-  //Create   and intialize   the   key
+  // Create and intialize the key
   cps_api_key_t   key;
   cps_api_key_from_attr_with_qual(&key,   BASE_PORT_INTERFACE_OBJ, cps_api_qualifier_OBSERVED);
 
-  // Create   the   object
+  // Create the object
   cps_api_object_t   obj   =  cps_api_object_create();
 
 
@@ -33,23 +33,20 @@ bool   cps_pub_intf_event()   {
     return   false;
   }
 
-
-  // Add   attributes   to   the   object
+  // Add attributes to the object
   cps_api_object_attr_add_u32(obj,BASE_PORT_INTERFACE_IFINDEX, if_nametoindex("e101-001-0"));
 
-
-  //Set   the   Key   to   the   object
+  // Set the key to the object
   cps_api_object_set_key(obj,&key);
 
-  //Publish   the   object
+  //Publish the object
   if(cps_api_event_publish(handle,obj)!=   cps_api_ret_code_OK){
     cps_api_object_delete(obj);
     return   false;
   }
-  // Delete   the   object
+  
+  // Delete the object
   cps_api_object_delete(obj);
   return   true;
 
 } 
-
-
