624c0fae5f18600f4c7ea1b6289c5cd9825585a1
Amy Buck <amy_buck@dell.com>
2016-10-20 07:59:05 -0700

    Update delete-acl.py

--- delete-acl.py
+++ delete-acl.py
@@ -1,16 +1,19 @@
+# Python block code to delete ACL
+
 import   cps_utils
 
-
-#Create   the   CPS   Object   and fill the   table-id   and entry-id   key values
+# Create the CPS object and fill the table-id and entry-id key values
 cps_obj   =  cps_utils.CPSObject(module='base-acl/entry', data={'table-id':   2, 'id':   1})
 
-#Associate   the   CPS   Object   with   a CPS   operation
+# Associate the CPS object with a CPS operation
 cps_update   =  ('delete',   cps_obj.get())
 
-#Add   the   CPS   object   to   a new   CPS   Transaction
+# Add the CPS object to a new CPS transaction
 cps_trans   =  cps_utils.CPSTransaction([cps_update])
 
-#Commit   the   CPS   transaction
+# Commit the CPS transaction
 r =  cps_trans.commit()
+
+# Check for failure
 if not   r:
     raise   RuntimeError   ("Error   deleting   ACL  Entry")
