5565080af15b78f6554bd971a0c06543e84b5804
Amy Buck <amy_buck@dell.com>
2016-10-24 09:36:35 -0700

    Update register-for-events.py

--- register-for-events.py
+++ register-for-events.py
@@ -2,8 +2,10 @@
 
 import   cps
 
+# Create handle to connect to event service
 handle   =  cps.event_connect()
 
+# Regidter key with event service
 cps.event_register(handle,   cps.key_from_name('observed','base-port/interface'))
 while   True:
     obj  =  cps.event_wait(handle)
