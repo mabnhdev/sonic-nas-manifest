60ecaa9d86b0500c0254edd66ecbfa96b3533a40
Amy Buck <amy_buck@dell.com>
2016-10-24 10:03:13 -0700

    Update python-event-subscriber-application.py

--- python-event-subscriber-application.py
+++ python-event-subscriber-application.py
@@ -3,9 +3,12 @@
 import cps
 import cps_utils
 
+# Create handle to connect with event service
 handle = cps.event_connect()
 
 while True:   
   ev = cps.event_wait(handle)
 
-  if ev[‘key’] == ...:       ...      elif ev['key'] == ...:       ...
+  if ev[‘key’] == ...:
+    ...      elif ev['key'] == ...:
+    ...
