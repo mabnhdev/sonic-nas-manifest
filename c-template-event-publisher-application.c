a3f22b1e5008328658268b308ae0ec6969e49fd9
Amy Buck <amy_buck@dell.com>
2016-10-24 10:06:34 -0700

    Update c-template-event-publisher-application.c

--- c-template-event-publisher-application.c
+++ c-template-event-publisher-application.c
@@ -7,14 +7,12 @@ cps_api_return_code_t   event_publish(cps_api_object_t   event_obj)
   static   bool init_flag   =  false;
   static   cps_api_event_service_handle_t   handle;
 
-
   if (!init_flag)   {
     /* Not initialized
 	=> Connect to CPS event subsystem */
     if (cps_api_event_service_init()   !=   cps_api_ret_code_OK)   {
       return   (cps_api_ret_code_ERR);
     }
-
 
     if (cps_api_event_client_connect(&handle)   !=
 	cps_api_ret_code_OK
@@ -33,6 +31,5 @@ cps_api_return_code_t   event_publish(cps_api_object_t   event_obj)
 
   /* Consume the given object */
   cps_api_object_delete(event_obj);
-
   return   (result);
 }
