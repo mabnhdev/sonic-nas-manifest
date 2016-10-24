303a3c11b6d465aefd9f44828fd484c71e329684
Amy Buck <amy_buck@dell.com>
2016-10-24 10:15:32 -0700

    Update c-template-event-subscriber-application.c

--- c-template-event-subscriber-application.c
+++ c-template-event-subscriber-application.c
@@ -6,19 +6,16 @@ and how to extract data from the GET response.
 
 cps_api_return_code_t   event_publish(cps_api_object_t   event_obj)
 {
-  static   bool                                                                    init_flag   =  false;
+  static   bool init_flag   =  false;
   static   cps_api_event_service_handle_t   handle;
-
 
   if (!init_flag)   {
     /* Not initialized
 	 => Connect to CPS event subsystem */
 
-
     if (cps_api_event_service_init()   !=   cps_api_ret_code_OK)   {
       return   (cps_api_ret_code_ERR);
     }
-
 
     if (cps_api_event_client_connect(&handle)   !=
 	cps_api_ret_code_OK
@@ -27,23 +24,19 @@ cps_api_return_code_t   event_publish(cps_api_object_t   event_obj)
     }
 
     /* Mark as initialized */
-
     init_flag   =  true;
   }
 
   cps_api_return_code_t   result; 
 
   /* Publish the given object */
-
   result   =  cps_api_event_publish(handle,   event_obj);
 
   /* Consume the given object */
-
-  cps_api_object_delete(event_obj);
+    cps_api_object_delete(event_obj);
 
   return   (result);
 }
-
 
 /************************************************************************ 
 Python Template: CPS Event Publisher Application
@@ -52,13 +45,11 @@ Python Template: CPS Event Publisher Application
 import   cps
 import   cps_utils
 
-
 handle   =  cps.event_connect()
 
   obj   =  cps_utils.CPSObject('base-port/interface',qual='observed', data=   {"ifindex":23})
 
   cps.event_send(handle,   obj.get())
-
 
 /************************************************************************ 
 Python Template: Event Subscriber Application
@@ -70,26 +61,23 @@ in the registration is used to determine the events that are delivered
 to this application (in this case, the list contains a single element).
 *************************************************************************/
 
-
   bool   event_handler(cps_api_object_t   object,   void   *context)
-{           /* Extract key and attributes of received object */
+{           
 
+  /* Extract key and attributes of received object */
   /* Do something with that information */
 }
   
-
 cps_api_return_code_t   event_subscribe()
-{           /* Connect to the CPS API event service */
-
+{           
+/* Connect to the CPS API event service */
     if (cps_api_event_service_init()   !=   cps_api_ret_code_OK)   {
       return   (cps_api_ret_code_ERR);
     } 
 
-
     if (cps_api_event_thread_init()   !=   cps_api_ret_code_OK)   {
       return   (cps_api_ret_code_ERR);
     }
-
 
     /* Register the event handler function */
     cps_api_key_t   key; cps_api_key_init(&key,    ); cps_api_event_reg_t   reg; reg.objects                            =  key;
@@ -99,11 +87,9 @@ cps_api_return_code_t   event_subscribe()
 	!=   cps_api_ret_code_OK
 	) {
       /* Failed to register handler */
-
       return   (cps_api_ret_code_ERR);
     }
 
     /* Indicate success */
-
     return   (cps_api_reg_code_OK);
 }
