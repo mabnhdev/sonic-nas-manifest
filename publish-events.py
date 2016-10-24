0876dcc857cfedb585980245f84cc120f0789751
Amy Buck <amy_buck@dell.com>
2016-10-24 09:40:09 -0700

    Update publish-events.py

--- publish-events.py
+++ publish-events.py
@@ -3,10 +3,11 @@
 import   cps
 import   cps_utils
 
-
+# Create handle to connect to event service
 handle   =  cps.event_connect()
 
-
+# Create CPS object
 obj   =  cps_utils.CPSObject('base-port/interface',qual='observed', data=   {"ifindex":23})
 
+# Publish the event
 cps.event_send(handle,   obj.get()) 
