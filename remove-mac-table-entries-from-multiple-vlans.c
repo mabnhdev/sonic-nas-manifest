9f4b468fba591e1d9041c8144f06a9461f04d6ff
Amy Buck <amy_buck@dell.com>
2016-10-24 09:35:00 -0700

    Update remove-mac-table-entries-from-multiple-vlans.c

--- remove-mac-table-entries-from-multiple-vlans.c
+++ remove-mac-table-entries-from-multiple-vlans.c
@@ -1,12 +1,12 @@
+// C application example to remove MAC address table entries from multiple VLANs
+
 #include   "cps_api_object.h"
 #include   "dell-base-l2-mac.h"
 #include   "cps_class_map.h"
 #include   "cps_api_object_key.h"
 
-
 #include   <stdint.h>
 #include   <net/if.h>
-
 
 bool   cps_flush_mac(){
 
@@ -31,17 +31,15 @@ bool   cps_flush_mac(){
   // Set the key for the object
   cps_api_object_set_key(obj,&key);
 
-  // Add attributes to flush MAC entries
+  // Add attributes to flush MAC table entries
   cps_api_attr_id_t   ids[3]   =  {BASE_MAC_FLUSH_INPUT_FILTER,0,   BASE_MAC_FLUSH_INPUT_FILTER_VLAN   };
   const   int   ids_len   =  sizeof(ids)/sizeof(ids[0]);
-
 
   uint16_t   vlan_list[3]={1,2,3};
   for(unsigned   int   ix=0;   ix<sizeof(vlan_list)/sizeof(vlan_list[0]);   ++ix) {
     ids[1]=ix;
     cps_api_object_e_add(obj,ids,ids_len,cps_api_object_ATTR_T_U16,&(vlan_list[ix]),sizeof(vlan_list[ix]));
   }
-
 
   unsigned   int   ifindex_list[]   =  { if_nametoindex("e101-001-0"),if_nametoindex("e101-002-0"), if_nametoindex("e101-003-0")};
   ids[2]=BASE_MAC_FLUSH_INPUT_FILTER_IFINDEX;
@@ -51,7 +49,6 @@ bool   cps_flush_mac(){
 
     cps_api_object_e_add(obj,ids,ids_len,cps_api_object_ATTR_T_U16,&ifindex_list[ix],sizeof(ifindex_list[ix]));
   }
-
 
   // Add the object along with the operation to transaction
   if(cps_api_action(&tran,obj)   !=   cps_api_ret_code_OK   ){
@@ -67,6 +64,5 @@ bool   cps_flush_mac(){
 
   // Cleanup the transaction
   cps_api_transaction_close(&tran);
-
   return   true;
 } 
